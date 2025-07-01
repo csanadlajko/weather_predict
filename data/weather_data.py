ab4008e588563147a74e711ede5cb75a244abd68
csanaed <csanilajko@gmail.com>
2025-07-01 22:15:14 +0200

    Created dataframe variable for location weather data

--- data/weather_data.py
+++ data/weather_data.py
@@ -1,1 +1,12 @@
-import meteostat
+from meteostat import Point, Daily
+from utils.DateFunctions import DateFunctions
+from utils.LocationFunctions import LocationFunctions
+import pandas as pd
+
+
+interval: DateFunctions = DateFunctions()
+target_city_model: LocationFunctions = LocationFunctions()
+target_city_point: Point = Point(target_city_model.latitude, target_city_model.longitude)
+
+data: Daily = Daily(target_city_point, interval.start_date, interval.end_date)
+df_data: pd.DataFrame = data.fetch()
--- utils/DateFunctions.py
+++ utils/DateFunctions.py
@@ -1,14 +1,19 @@
 import datetime
+from utils.CommonConstants import CommonConstants
 
 class DateFunctions():
     COMMON_DATE_FORMAT: str = "%Y.%m.%d"
     
-    def parseStringDate(string_date: str) -> datetime.datetime:
+    def __init__(self, start_date: str = CommonConstants.START_DATE, end_date: str = CommonConstants.END_DATE):
+        self.start_date = self.parseStringDate(start_date)
+        self.end_date = self.parseStringDate(end_date)
+    
+    def parseStringDate(self, string_date: str) -> datetime:
         """
         Parses date if its format matches %Y.%m.%d.
         """
         try:
-            parsed_date: datetime.datetime = datetime.datetime.strptime(string_date, DateFunctions.COMMON_DATE_FORMAT)
+            parsed_date: datetime = datetime.datetime.strptime(string_date, DateFunctions.COMMON_DATE_FORMAT)
             return parsed_date
         except Exception as e:
             print(f"Couldn't parse date, an error has occured: {e}")
