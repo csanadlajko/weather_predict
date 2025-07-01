2a99021804aeb988bf9b9b1ad926d6fb09c5d064
csanaed <csanilajko@gmail.com>
2025-07-01 22:23:10 +0200

    Added dataclasses for weather and wind data

--- models/weather_model.py
+++ models/weather_model.py
@@ -1,9 +1,16 @@
 from dataclasses import dataclass
 from datetime import date
+from pandas import Float64Dtype
 
 @dataclass(slots=True)
-class Weather:
+class WeatherData:
     date: date
-    temperature: float
-    humidity: float
-    
+    avg_temperature: Float64Dtype
+    min_temperature: Float64Dtype
+    snow: Float64Dtype
+
+@dataclass(slots=True)
+class WindData:
+    wind_speed: Float64Dtype
+    wind_peak_gust: Float64Dtype
+    wind_direction: Float64Dtype
