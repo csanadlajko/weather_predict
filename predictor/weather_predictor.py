3f84743e4eb25fe7023c16f60fc1eaf9feefd865
csanaed <csanilajko@gmail.com>
2025-07-02 22:51:44 +0200

    Initialized temorary linear regression model for weather prediction

--- predictor/weather_predictor.py
+++ predictor/weather_predictor.py
@@ -1,4 +1,14 @@
 from sklearn.linear_model import LinearRegression
 import pandas as pd
 import numpy as np
+from process.features import final_df
 
+X: pd.DataFrame = final_df.drop(columns=["tavg"])
+Y: pd.DataFrame = final_df["tavg"]
+
+lin_reg_model: LinearRegression = LinearRegression()
+
+lin_reg_model.fit(X, Y)
+y_pred = lin_reg_model.predict(X)
+
+## TODO: SPLIT DATA, USE OTHER MODELS AS WELL
