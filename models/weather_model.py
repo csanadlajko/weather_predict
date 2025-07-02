3d34eb6266eebcc757697ab6ff3629e276e0621f
csanaed <csanilajko@gmail.com>
2025-07-02 21:44:03 +0200

    Initialized dataclasses in weather data file

--- models/weather_model.py
+++ models/weather_model.py
@@ -7,6 +7,7 @@ class WeatherData:
     date: date
     avg_temperature: Float64Dtype
     min_temperature: Float64Dtype
+    max_temperature: Float64Dtype
     snow: Float64Dtype
 
 @dataclass(slots=True)
@@ -14,3 +15,7 @@ class WindData:
     wind_speed: Float64Dtype
     wind_peak_gust: Float64Dtype
     wind_direction: Float64Dtype
+    
+@dataclass(slots=True)
+class SunData:
+    sunny_hours: Float64Dtype
